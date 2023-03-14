8bddb5f52fa5cff72652c05d3a044e779a2d6a3c
kizwolak <ki.zwolak@gmail.com>
2023-03-14 18:36:08 -0300

    Change background colour and alignment

--- src/components/HomeProjects.js
+++ src/components/HomeProjects.js
@@ -44,7 +44,7 @@ export default function HomeProjects() {
       <h2 className="projects-subtitle">And so much more!</h2>
       <p className="projects-invitation">
         Please visit the Projects page on the navigation bar or check out my
-        GitHub page to see what else I have been up to!
+        GitHub to see what else I have been up to!
       </p>
       <h3>Hope to hear from you soon!</h3>
     </div>
