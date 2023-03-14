9e9ebee283446c742f676a9469cc0a23bf40f6d2
kizwolak <ki.zwolak@gmail.com>
2023-03-14 12:22:51 -0300

    Animate icons individually

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,16 +8,16 @@ export default function Navbar() {
         <button>Projects</button>
       </div>
       <div className="navbar-img">
-        <a href="https://github.com/kizwolak">
+        <a href="https://github.com/kizwolak" id="first-nav-img">
           <img src="./images/github-mark.svg" alt="Github" />
         </a>
-        <a href="https://github.com/kizwolak">
+        <a href="https://github.com/kizwolak" id="sec-nav-img">
           <img
             src="./images/email-black-envelope-back-svgrepo-com.svg"
             alt="Github"
           />
         </a>
-        <a href="https://github.com/kizwolak">
+        <a href="https://github.com/kizwolak" id="thrd-nav-img">
           <img src="./images/iconmonstr-linkedin-3.svg" alt="Github" />
         </a>
       </div>
